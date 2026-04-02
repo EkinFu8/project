ac3fd75cb5ec020f634743e60645130c78307a40
Max Wainwright <mwainwright@buckley.org>
2026-04-02 15:42:44 -0400

    Added Dashboard
    Not sure if we want this, if not pretty easy to get rid of, but could look good for now/ for prototype.

--- apps/web/src/App.tsx
+++ apps/web/src/App.tsx
@@ -4,12 +4,14 @@ import HeroPage from "./pages/hero";
 import UnderwriterPage from "./pages/underwriter";
 import BusinessAnalyst from "@/pages/business-analyst.tsx";
 import EmployeesFormPage from "@/pages/employees-form.tsx";
+import DashboardPage from "@/pages/dashboard.tsx";
 
 const navItems = [
   { label: "Hero", to: "/hero" },
   { label: "Underwriter", to: "/underwriter" },
   { label: "Business Analyst", to: "/businessAnalyst" },
   { label: "Employees", to: "/employees" },
+  { label: "Dashboard", to: "/dashboard" },
 ];
 
 function App() {
@@ -23,6 +25,7 @@ function App() {
           <Route path="/underwriter" element={<UnderwriterPage />} />
           <Route path="/businessAnalyst" element={<BusinessAnalyst/>} />
           <Route path="/employees" element={<EmployeesFormPage />} />
+          <Route path="/dashboard" element={<DashboardPage />} />
         </Routes>
       </div>
     </BrowserRouter>
