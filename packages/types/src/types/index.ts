3fbbad2517f6b63cb9b344e612f69ba3f5b97e32
Myantonomo Garcia <myantonomo.cacique@gmail.com>
2026-04-07 23:42:14 -0400

    i thought i needed a login.ts types file but apparently i dont

--- packages/types/src/types/index.ts
+++ packages/types/src/types/index.ts
@@ -1,4 +1,3 @@
 export type { ContentItem, ContentWithEmployee } from "./content";
 export type { Employee, EmployeeWithContent } from "./employee";
-export type { Login } from "./login";
 export type { User } from "./user";
