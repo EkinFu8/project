a42987be4feb8e4cb16eed25264260efd319be54
Midna18 <maxclarke989@gmail.com>
2026-04-27 20:27:26 -0400

    5

--- apps/admin/src/App.tsx
+++ apps/admin/src/App.tsx
@@ -116,7 +116,6 @@ function App() {
           <Route path="/users/new" element={<UserFormPage />} />
           <Route path="/users/:id" element={<UserFormPage />} />
           <Route path="/account" element={<AccountPage />} />
-          <Route path="/about" element={<AboutPage />} />
           <Route path="*" element={<Navigate to="/content" replace />} />
         </Route>
       </Routes>
